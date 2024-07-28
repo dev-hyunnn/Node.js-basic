b88bdf96a113161cfe5c84ef492f523e638ea625
SeongHyun <134840343+dev-hyunnn@users.noreply.github.com>
2024-07-28 18:45:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,68 @@
 var http = require('http');
 var fs = require('fs');
-var app = http.createServer(function(request,response) {
-    var url = request.url;
-    if(url == '/') {
-        url = '/index.html';
+var url = require('url');
+ 
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/') {
+        if(queryData.id === undefined) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        }
+        else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+                var title = queryData.id;
+                var template = `
+                <!doctype html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                    </ul>
+                    <h2>${title}</h2>
+                    <p>${description}</p>
+                </body>
+                </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+            });
+        }
     }
-    if(url == '/favicon.ico') {
+    else {
         response.writeHead(404);
-        response.end();
-        return;
+        response.end('Not found');
     }
-    response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + url));
 });
-app.listen(3000); 
+app.listen(3000);
